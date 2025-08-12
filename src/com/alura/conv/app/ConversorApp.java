74d7c0e31db2d3cfe2d795f8c7e99c15fc6d793f
WAV9874 <acosta_vela_wilmer@hotmail.com>
2025-08-11 19:54:23 -0500

    feat: versión inicial del conversor de monedas

---
+++ src/com/alura/conv/app/ConversorApp.java
@@ -0,0 +1,11 @@
+package com.alura.conv.app;
+
+import com.alura.conv.core.Conversor;
+
+public class ConversorApp {
+    public static void main(String[] args) {
+        new Conversor().menu();
+    }
+}
+
+
